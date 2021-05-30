e733edae472172ba81d58b1e5a1f000d35c6d885
imvickykumar999 <imvickykumar999@gmail.com>
2021-05-30 14:11:11 +0530

    vid as title

--- vicks/ytdownload.py
+++ vicks/ytdownload.py
@@ -17,7 +17,8 @@ def yt_video(vid = 'KBtk5FUeJbk'):
         youtube_video_url = 'https://youtu.be/' + vid
         yt_obj = YouTube(youtube_video_url)
 
-        yt_obj.title = ''.join([i for i in yt_obj.title if i.isalpha()])
+        # yt_obj.title = ''.join([i for i in yt_obj.title if i.isalpha()])
+        yt_obj.title = vid
         filters = yt_obj.streams.filter(progressive=True, file_extension='mp4')
 
         filters.get_highest_resolution().download("uploads/")
@@ -26,7 +27,7 @@ def yt_video(vid = 'KBtk5FUeJbk'):
     except Exception as e:
         print(e)
 
-    return ''.join([i for i in yt_obj.title if i.isalpha()])
+    return vid
 
 # youtube_video_url = 'https://youtu.be/GbqGmNsWDaE'
 # yt_obj = YouTube(youtube_video_url)
